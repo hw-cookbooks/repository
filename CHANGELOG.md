f94da337ba291df5d0ba0d6914326cefafd6e147
Chris Roberts <chrisroberts.code@gmail.com>
2013-04-15 07:58:05 -0700

    Update for version release v0.1.2

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,2 +1,7 @@
+## v0.1.2
+* Ensure apache configuration is properly enabled
+* Add mixlib shellout usage in place of directly shelling
+* Add `-i` option to sudo calls
+
 ## v0.1.0
 * Initial release
--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,8 @@
 name 'repository'
-version '0.1.1'
+version '0.1.2'
 maintainer 'Chris Roberts'
+maintainer_email 'chrisroberts.code@gmail.com'
+license 'Apache 2.0'
 
 depends 'apache2'
 depends 'apt'
